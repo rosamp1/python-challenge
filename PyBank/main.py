3b820b35ac2c4d45b2b793fb7c2ec088e28c5c1a
rosamp1 <132237292+rosamp1@users.noreply.github.com>
2023-07-15 17:25:13 -0500

    added instructions to PyBank file

---
+++ PyBank/main.py
@@ -0,0 +1,8 @@
+# Import budget_data.csv file
+# Create a python script that analyzes the records to calculate each of the foloowing values:
+# The toal number of months included in the dataset
+# THe net total amount of "Profit/Losses" over the entire period
+# The changed in "Profit/Losses" over the entire perios, and then the average of thise changes
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in profits 9date and amount) over the entire period
+# Final script should both print the analysis to the terminal and export a text file with the results
