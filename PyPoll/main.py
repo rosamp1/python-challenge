2272c67e9751560f3bf647aacbfcd6713a65d124
rosamp1 <132237292+rosamp1@users.noreply.github.com>
2023-07-18 16:22:41 -0500

    added final changes and assignment is now complete

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,30 +1,50 @@
-# Import budget_data.csv file
+# Followed Instructor led content and used Chat GPT for further assitance
 import os
 import csv
 
-
-csvpath = os.path.join("Resources","election_data.csv")
+csvpath = os.path.join("Resources", "election_data.csv")
+output_file = "election_results.txt"
 
 with open(csvpath) as csvfile:
-    
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
-
-# The total number of votes cast
+    # Total number of votes cast , complete list of candidates
     rowcount = 0
-    
+    votes_per_candidate = {}
 
     for row in csvreader:
         rowcount += 1
+        candidate = row[2]  # Assuming column C is for candidates
+        votes_per_candidate[candidate] = votes_per_candidate.get(candidate, 0) + 1
 
-    print(f"Total number of votes:{rowcount}")
+    total_votes = sum(votes_per_candidate.values())
+
+#Assistance from Chat GPT on exporting results onto text file 
+    with open(output_file, "w") as file:
+        file.write("Election Results\n")
+        file.write("-------------------------\n")
+        file.write(f"Total number of votes: {rowcount}\n")
+        file.write("-------------------------\n")
+        file.write("Candidates who received votes:\n")
+
+# percentafe of votes and total number of votes , print winner
+
+        for candidate, votes in votes_per_candidate.items():
+            vote_percentage = (votes / total_votes) * 100
+            result = f"{candidate}: {votes} votes ({vote_percentage:.3f}% of total votes)\n"
+            file.write(result)
+            print(result, end="")
+
+        file.write("-------------------------\n")
+        winner = max(votes_per_candidate, key=votes_per_candidate.get)
+        result = f"Winner: {winner}\n"
+        file.write(result)
+        print(result)
+
+    print(f"Results exported to '{output_file}'")
 
 
 
-# The total number of votes cast
-# A complete list of candidates who received votes
-# The percentage of votes each candidate won
-# The total number of votes each candidate won
-# The winner of the election based on popular vote
+
 
